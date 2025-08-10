bc79a85ab27b792ec0f0ed6b02fda26a2bd22f69
hamzabrn0 <barrann24@gmail.com>
2025-08-10 17:07:16 +0300

    Fix User model filename case issue

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
-const User = require("../models/User");
+const User = require("../models/user");
 const { authenticateToken } = require("../middleware/auth");
 
 const router = express.Router();
